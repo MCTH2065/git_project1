85f98f7e0ffb1447bcbe3df8ca4b4db372455d95
Matt <cor_n@mail.ru>
2021-11-24 23:00:14 +0300

    Ne w n ew

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('My first change in git program')
     print('And I change it every day')
+    print('Ahhh s***, here we go again')
 
 
 if __name__ == '__main__':
